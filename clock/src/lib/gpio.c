8cc029eaa50ba13d93de194ac2905cb7a0e0e238
Sadamingh <yxing11@usfca.edu>
2021-01-07 05:51:18 +0800

    complete GPIO model

--- clock/src/lib/gpio.c
+++ clock/src/lib/gpio.c
@@ -1,8 +1,9 @@
 #include "gpio.h"
 
 static volatile unsigned int *FSEL_BASE =  (unsigned int *)0x20200000;
-//static volatile unsigned int *SET0  =  (unsigned int *)0x2020001c;
-//static volatile unsigned int *CLR0  =  (unsigned int *)0x20200028;
+static volatile unsigned int *SET_BASE  =  (unsigned int *)0x2020001c;
+static volatile unsigned int *CLR_BASE  =  (unsigned int *)0x20200028;
+static volatile unsigned int *LEV_BASE  =  (unsigned int *)0x20200034 ;
 
 void gpio_init(void) {
 }
@@ -10,25 +11,26 @@ void gpio_init(void) {
 void gpio_set_function(unsigned int pin, unsigned int function) {
 	// If `pin` or `function` is invalid, does nothing.
 	if (!(pin >= GPIO_PIN_FIRST && pin <= GPIO_PIN_LAST)) return;
-	if (!(function >= GPIO_FUNC_INPUT && function <= GPIO_FUNC_ALT5)) return; 
+	if (!(function >= GPIO_FUNC_INPUT && function <= GPIO_FUNC_ALT3)) return; 
 
 	// Get the FSEL based on the pin.
-	unsigned int *FSEL = (unsigned int *) (FSEL_BASE + (pin / 10) * 4);
+	volatile unsigned int *FSEL = FSEL_BASE + (pin / 10);
 
-	// Settings for other pins should be unchanged.
-	volatile unsigned int temp = *FSEL;
+	// Settings for other pins should be unchanged. 
+	// Use a mask to clear the set value.
+	int offset = (pin % 10) * 3;
+	*FSEL = *FSEL & ~(0b111 << offset);
 
 	// Set a GPIO function for GPIO pin number `pin`.
-	int offset = (pin % 10) * 3;
-	*FSEL = temp | (function << offset);
+	*FSEL = *FSEL | (function << offset);
 }
 
 unsigned int gpio_get_function(unsigned int pin) {
-	// If `pin` is invalid, does nothing.
-	if (!(pin >= GPIO_PIN_FIRST && pin <= GPIO_PIN_LAST)) return -1;
+	// If `pin` is invalid, returns GPIO_INVALID_REQUEST.
+	if (!(pin >= GPIO_PIN_FIRST && pin <= GPIO_PIN_LAST)) return GPIO_INVALID_REQUEST;
 
 	// Get the FSEL and offset based on the pin.
-	unsigned int *FSEL = (unsigned int *) (FSEL_BASE + (pin / 10) * 4);
+	volatile unsigned int *FSEL = FSEL_BASE + (pin / 10);
 	int offset = (pin % 10) * 3;
 
 	// Return the function of the given pin.
@@ -44,9 +46,33 @@ void gpio_set_output(unsigned int pin) {
 }
 
 void gpio_write(unsigned int pin, unsigned int value) {
-    // TODO: Your code goes here.
+	// If `pin` is invalid, does nothing.
+	if (!(pin >= GPIO_PIN_FIRST && pin <= GPIO_PIN_LAST)) return;
+
+	// Get the offset based on the pin.
+	int offset = pin % 32;
+
+	// Find out whether set or clear and assign the value.
+	if (value) {
+		volatile unsigned int *reg = SET_BASE + (pin / 32);
+		// Set the level value for GPIO pin number `pin`.
+		*reg = (1 << offset);
+	}
+	else {
+		volatile unsigned int *reg = CLR_BASE + (pin / 32);
+		// Set the level value for GPIO pin number `pin`.
+		*reg = (1 << offset);
+	}
 }
 
 unsigned int gpio_read(unsigned int pin) {
-    return 0;  // TODO: Your code goes here.
+	// If `pin` is invalid, returns GPIO_INVALID_REQUEST.
+	if (!(pin >= GPIO_PIN_FIRST && pin <= GPIO_PIN_LAST)) return GPIO_INVALID_REQUEST;
+
+	// Get the LEV registor and offset based on the pin.
+	volatile unsigned int *LEV = LEV_BASE + (pin / 32);
+	int offset = pin % 32;
+
+	// Return current level (1 for high, 0 for low) for GPIO pin.
+	return (*LEV >> offset) & 0b1;
 }
--- clock/src/tests/test_gpio_timer.c
+++ clock/src/tests/test_gpio_timer.c
@@ -14,6 +14,69 @@ void test_gpio_set_get_function(void) {
     // Test get pin function after setting
     assert( gpio_get_function(GPIO_PIN2) == GPIO_FUNC_OUTPUT );
 
+    // Confirm setting a GPIO pin to alternate function modes, 
+    // not just input or output
+    // test 1
+    gpio_set_function(GPIO_PIN2, GPIO_FUNC_INPUT);
+    assert( gpio_get_function(GPIO_PIN2) == GPIO_FUNC_INPUT );
+    // test 2
+    gpio_set_function(GPIO_PIN2, GPIO_FUNC_ALT5);
+    assert( gpio_get_function(GPIO_PIN2) == GPIO_FUNC_ALT5 );
+    // test 3
+    gpio_set_function(GPIO_PIN2, GPIO_FUNC_ALT0);
+    assert( gpio_get_function(GPIO_PIN2) == GPIO_FUNC_ALT0 );
+    // test 4
+    gpio_set_function(GPIO_PIN2, GPIO_FUNC_ALT3);
+    assert( gpio_get_function(GPIO_PIN2) == GPIO_FUNC_ALT3 );
+
+    // Confirm can configure a GPIO pin in each of the different
+    // FSEL registers
+    gpio_set_output(GPIO_PIN12);
+    assert( gpio_get_function(GPIO_PIN12) == GPIO_FUNC_OUTPUT );
+
+    gpio_set_output(GPIO_PIN22);
+    assert( gpio_get_function(GPIO_PIN22) == GPIO_FUNC_OUTPUT );
+
+    gpio_set_output(GPIO_PIN32);
+    assert( gpio_get_function(GPIO_PIN32) == GPIO_FUNC_OUTPUT );
+
+    gpio_set_output(GPIO_PIN42);
+    assert( gpio_get_function(GPIO_PIN42) == GPIO_FUNC_OUTPUT );
+
+    gpio_set_output(GPIO_PIN52);
+    assert( gpio_get_function(GPIO_PIN52) == GPIO_FUNC_OUTPUT );
+
+    // Confirm no interference when configuring multiple GPIO 
+    // pins within the same FSEL register
+    gpio_set_output(GPIO_PIN12);
+    gpio_set_output(GPIO_PIN13);
+    gpio_set_output(GPIO_PIN14);
+    assert( gpio_get_function(GPIO_PIN12) == GPIO_FUNC_OUTPUT );
+    assert( gpio_get_function(GPIO_PIN13) == GPIO_FUNC_OUTPUT );
+    assert( gpio_get_function(GPIO_PIN14) == GPIO_FUNC_OUTPUT );
+
+    // Confirm that improper requests are gracefully handled
+    assert( gpio_get_function(70) == GPIO_INVALID_REQUEST );
+    assert( gpio_get_function(-10) == GPIO_INVALID_REQUEST );
+
+    // Random tests
+    gpio_set_function(GPIO_PIN31, GPIO_FUNC_ALT5);
+    gpio_set_function(GPIO_PIN24, GPIO_FUNC_ALT3);
+    gpio_set_function(GPIO_PIN50, GPIO_FUNC_ALT2);
+    assert( gpio_get_function(GPIO_PIN31) == GPIO_FUNC_ALT5 );
+    assert( gpio_get_function(GPIO_PIN24) == GPIO_FUNC_ALT3 );
+    assert( gpio_get_function(GPIO_PIN50) == GPIO_FUNC_ALT2 );
+    gpio_set_function(GPIO_PIN11, GPIO_FUNC_ALT5);
+    gpio_set_function(GPIO_PIN12, GPIO_FUNC_ALT3);
+    gpio_set_function(GPIO_PIN13, GPIO_FUNC_ALT2);
+    gpio_set_function(GPIO_PIN16, GPIO_FUNC_ALT4);
+    gpio_set_function(GPIO_PIN17, GPIO_FUNC_OUTPUT);
+    assert( gpio_get_function(GPIO_PIN11) == GPIO_FUNC_ALT5 );
+    assert( gpio_get_function(GPIO_PIN12) == GPIO_FUNC_ALT3 );
+    assert( gpio_get_function(GPIO_PIN13) == GPIO_FUNC_ALT2 );
+    assert( gpio_get_function(GPIO_PIN16) == GPIO_FUNC_ALT4 );
+    assert( gpio_get_function(GPIO_PIN17) == GPIO_FUNC_OUTPUT );
+
 }
 
 void test_gpio_read_write(void) {
@@ -22,7 +85,23 @@ void test_gpio_read_write(void) {
 
     // Test gpio_write low, then gpio_read
     gpio_write(GPIO_PIN20, 0);
-    assert( gpio_read(GPIO_PIN20) ==  0 );
+    assert( gpio_read(GPIO_PIN20) ==  0 );gpio_set_function(GPIO_PIN31, GPIO_FUNC_ALT5);
+    gpio_set_function(GPIO_PIN24, GPIO_FUNC_ALT3);
+    gpio_set_function(GPIO_PIN50, GPIO_FUNC_ALT2);
+    assert( gpio_get_function(GPIO_PIN31) == GPIO_FUNC_ALT5 );
+    assert( gpio_get_function(GPIO_PIN24) == GPIO_FUNC_ALT3 );
+    assert( gpio_get_function(GPIO_PIN50) == GPIO_FUNC_ALT2 );
+    //many pins in same register
+    gpio_set_function(GPIO_PIN11, GPIO_FUNC_ALT5);
+    gpio_set_function(GPIO_PIN12, GPIO_FUNC_ALT3);
+    gpio_set_function(GPIO_PIN13, GPIO_FUNC_ALT2);
+    gpio_set_function(GPIO_PIN16, GPIO_FUNC_ALT4);
+    gpio_set_function(GPIO_PIN17, GPIO_FUNC_OUTPUT);
+    assert( gpio_get_function(GPIO_PIN11) == GPIO_FUNC_ALT5 );
+    assert( gpio_get_function(GPIO_PIN12) == GPIO_FUNC_ALT3 );
+    assert( gpio_get_function(GPIO_PIN13) == GPIO_FUNC_ALT2 );
+    assert( gpio_get_function(GPIO_PIN16) == GPIO_FUNC_ALT4 );
+    assert( gpio_get_function(GPIO_PIN17) == GPIO_FUNC_OUTPUT );
 
    // Test gpio_write high, then gpio_read
     gpio_write(GPIO_PIN20, 1);
